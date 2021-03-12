0dbca3dd0c61ea5d4afa53e076e104815b22d172
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-03-11 16:02:18 -0800

    Use values() method with enums.

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
+}
+
+public class Main { 
+  public static void main(String[] args) { 
+    for (Level myVar : Level.values()) {
+      System.out.println(myVar);
+    }
+  } 
+}
